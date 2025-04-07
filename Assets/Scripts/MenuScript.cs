cb8f56308d30d9c65d773bb338ecc04d23df7f88
Jared Miller <jaredmiller219@gmail.com>
2025-04-07 18:29:39 -0400

    Refactor Menu Scene and Update Quit Functionality
    - Removed the old quitscript.cs and its associated meta file.
    - Created a new MenuScript.cs to handle quitting and level selection.
    - Updated the Menu scene to reflect new button names and positions.
    - Adjusted button functionality to link to the new level select scene.
    - Changed the version of com.unity.burst package in packages-lock.json.
    - Added Menu scene to Editor Build Settings.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Quitscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Quit()
+    {
+        // quit the game
+        Application.Quit();
+
+        // if we are in the editor, stop playing
+        // JUST FOR TESTING
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void LevelSelect()
+    {
+        // load the level select scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CSS-Dustin");
+    }
+
+}
