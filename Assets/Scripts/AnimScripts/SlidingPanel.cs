9dd97364fe47922215139befc1673db28a5c8c0d
Jared Miller <jaredmiller219@gmail.com>
2025-04-10 11:37:04 -0400

    updated side panel animation and sizing

--- Assets/Scripts/AnimScripts/SlidingPanel.cs
+++ Assets/Scripts/AnimScripts/SlidingPanel.cs
@@ -19,38 +19,14 @@ public class SlidePanelController : MonoBehaviour
     private void Start()
     {
         _animator = Panel.GetComponent<Animator>();
-        // _panelRect = GetComponent<RectTransform>();
-
-        // _openPos = new Vector2(178.99f, _panelRect.anchoredPosition.y);
-        // var panelWidth = _panelRect.rect.width;
-        // _closedPos = new Vector2(178.99f - panelWidth + visibleTabWidth, _panelRect.anchoredPosition.y);
-
-        // _panelRect.anchoredPosition = _closedPos;
     }
 
     public void TogglePanel()
     {
-        // StopAllCoroutines();
-        // StartCoroutine(Slide(_isOpen ? _closedPos : _openPos));
-        // _isOpen = !_isOpen;
         // toggle the animator's "open" bool
         var isOpen = _animator.GetBool(Open);
         // set the animator's "open" bool to the opposite of its current value
         _animator.SetBool(Open, !isOpen);
 
     }
-
-    // private System.Collections.IEnumerator Slide(Vector2 target)
-    // {
-    //     while (Vector2.Distance(_panelRect.anchoredPosition, target) > 0.1f)
-    //     {
-    //         _panelRect.anchoredPosition = Vector2.Lerp(
-    //             _panelRect.anchoredPosition,
-    //             target,
-    //             Time.deltaTime * slideSpeed
-    //         );
-    //         yield return null;
-    //     }
-    //     _panelRect.anchoredPosition = target;
-    // }
 }
