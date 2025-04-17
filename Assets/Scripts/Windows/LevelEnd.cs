d31503aaf50153e5f2fed891fc187868fd6986f3
Jared Miller <jaredmiller219@gmail.com>
2025-04-17 19:18:03 -0400

    updated level end buttons to now work

--- Assets/Scripts/Windows/LevelEnd.cs
+++ Assets/Scripts/Windows/LevelEnd.cs
@@ -17,6 +17,11 @@ public class LevelEnd : MonoBehaviour
     public GameObject restartBtn;
 
     /// <summary>
+    /// Reference to the menu button GameObject.
+    /// </summary>
+    public GameObject menuBtn;
+
+    /// <summary>
     /// Reference to the Notepad manager script, which manages the game's challenges.
     /// </summary>
     private Notepad _notepadManager;
@@ -27,12 +32,20 @@ public class LevelEnd : MonoBehaviour
         // Find the GameObject named "NotepadManager" in the scene.
         // This assumes there is a GameObject in the scene with the exact name "NotepadManager".
         // Get the Notepad component attached to that GameObject and assign it to _notepadManager.
-        _notepadManager = GameObject.Find("NotepadManager").GetComponent<Notepad>();
+        if (GameObject.Find("NotepadManager") != null)
+        {
+            _notepadManager = GameObject.Find("NotepadManager").GetComponent<Notepad>();
+        }
 
         // Access the Button component of the restartBtn GameObject.
         // Add a listener to the button's onClick event.
         // When the button is clicked, the RestartGame method will be executed.
         restartBtn.GetComponent<Button>().onClick.AddListener(RestartGame);
+
+        // Access the Button component of the menuBtn GameObject.
+        // Add a listener to the button's onClick event.
+        // When the button is clicked, the GoToMenu method will be executed.
+        menuBtn.GetComponent<Button>().onClick.AddListener(GoToMenu);
     }
 
     // Method to restart the game.
@@ -53,5 +66,21 @@ public class LevelEnd : MonoBehaviour
         // The following line would reload the current scene, effectively restarting the game.
         // Uncomment this line if you want the scene to reload when restarting:
         // UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    // Method to go to the main menu.
+    private void GoToMenu()
+    {
+        // Log a message to the console indicating that the game is going to the menu.
+        // This is useful for debugging purposes.
+        Debug.Log("Going to Menu...");
+
+        // Deactivate the completePopup GameObject.
+        // This hides the popup from the screen by setting its active state to false.
+        completePopup.SetActive(false);
+
+        // The following line would load the main menu scene.
+        // Uncomment this line if you want to load the main menu when going to it:
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
     }
 }
