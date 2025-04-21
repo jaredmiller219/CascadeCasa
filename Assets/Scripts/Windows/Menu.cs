05ce805bca4187356911fad0e2974712aac93f7e
Jared Miller <jaredmiller219@gmail.com>
2025-04-21 16:44:01 -0400

    updated

--- Assets/Scripts/Windows/Menu.cs
+++ Assets/Scripts/Windows/Menu.cs
@@ -1,53 +1,84 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
+    [Header("Buttons (Parents with Button Component)")]
+    public GameObject levelSelectButton;
+    public GameObject playButton;
+    public GameObject instructionsButton;
 
-    /// <summary>
-    /// This method is called to quit the game.
-    /// It closes the application when running in a standalone build.
-    /// </summary>
-    /// <remarks>
-    /// This method uses Application.Quit() to close the application.
-    /// In the Unity Editor, it stops the play mode instead of quitting.
-    /// This is useful for testing purposes since Application.Quit() does not work in the editor.
-    /// </remarks>
+    private TMP_Text levelSelectText;
+    private TMP_Text playText;
+    private TMP_Text instructionsText;
+
+    private void Start()
+    {
+        // Get TMP_Text from each button’s children
+        levelSelectText = levelSelectButton.GetComponentInChildren<TMP_Text>();
+        playText = playButton.GetComponentInChildren<TMP_Text>();
+        instructionsText = instructionsButton.GetComponentInChildren<TMP_Text>();
+    }
+
+    public void OnLevelSelectPress()
+    {
+        SetPressedColor(levelSelectText);
+    }
+
+    public void OnLevelSelectRelease()
+    {
+        SetDefaultColor(levelSelectText);
+        LevelSelect();
+    }
+
+    public void OnPlayPress()
+    {
+        SetPressedColor(playText);
+    }
+
+    public void OnPlayRelease()
+    {
+        SetDefaultColor(playText);
+    }
+
+    public void OnInstructionsPress()
+    {
+        SetPressedColor(instructionsText);
+    }
+
+    public void OnInstructionsRelease()
+    {
+        SetDefaultColor(instructionsText);
+    }
+
+    private void SetPressedColor(TMP_Text text)
+    {
+        if (text != null)
+            text.color = new Color32(200, 200, 200, 255); // Light gray
+    }
+
+    private void SetDefaultColor(TMP_Text text)
+    {
+        if (text != null)
+            text.color = new Color32(255, 255, 255, 255); // White
+    }
+
     public void Quit()
     {
-        // Quit the application. This works when the game is built and running as a standalone application.
         Application.Quit();
-
-        // If the game is running in the Unity Editor, stop playing the game.
-        // This is useful for testing purposes since Application.Quit() does not work in the editor.
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // Stop the play mode in the Unity Editor.
+        UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
 
-    /// <summary>
-    /// This method is called to start the level select screen.
-    /// It loads the level select screen where players can choose a level to play.
-    /// </summary>
-    /// <remarks>
-    /// This method uses the SceneManager to load the scene named "LevelSelect".
-    /// It assumes that the scene is added to the build settings.
-    /// </remarks>
     public void LevelSelect()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelect");
+        SceneManager.LoadScene("LevelSelect");
     }
 
-
-    /// <summary>
-    /// This method is called to start the settings screen.
-    /// It loads the settings screen where players can adjust game settings.
-    /// </summary>
-    /// <remarks>
-    /// This method uses the SceneManager to load the scene named "Settings".
-    /// It assumes that the scene is added to the build settings.
-    /// </remarks>
     public void Settings()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Settings");
+        SceneManager.LoadScene("Settings");
     }
 }
