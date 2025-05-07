5c3a07ad15f71437bd901dc0bd8dbd4126765537
Jared Miller <jaredmiller219@gmail.com>
2025-05-07 14:30:31 -0400

    updated

--- Assets/Scripts/Menus/Instructions.cs
+++ Assets/Scripts/Menus/Instructions.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Instructions : MonoBehaviour
 {
+    public GameObject instructionText;
+
     public void BackToMenu()
     {
         // Load the main menu scene
         UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+
+    public void SetText(TMPro.TMP_Text newText)
+    {
+        instructionText.GetComponent<TMPro.TMP_Text>().text = newText.ToString();
     }
 }
