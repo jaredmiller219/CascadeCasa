e8228e58446d856a61587c60fcad6dc36b9c4279
Jared Miller <jaredmiller219@gmail.com>
2025-05-07 22:43:06 -0400

    updated scroll view

--- Assets/Scripts/Menus/Instructions.cs
+++ Assets/Scripts/Menus/Instructions.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Instructions : MonoBehaviour
 {
+
+    // public GameObject instructionText; // Text display area
+    public GameObject buttonPrefab;    // Assign your button prefab in the Inspector
+    public Transform buttonContainer;  // A UI parent object (e.g., Vertical Layout Group)
+
     public GameObject instructionText;
 
-    public GameObject action1;
+    public Vector2 buttonSize = new(650f, 100f); // Width x Height
 
-    public GameObject action2;
+    // public GameObject action1;
 
-    public GameObject action3;
+    // public GameObject action2;
 
-    public GameObject action4;
+    // public GameObject action3;
+
+    // public GameObject action4;
 
     private readonly List<string> _instructions = new()
     {
@@ -31,6 +39,26 @@ public class Instructions : MonoBehaviour
     public void Start()
     {
         instructionText.GetComponent<TMP_Text>().text = "";
+
+        float startY = 0f;
+
+        for (int i = 0; i < _instructions.Count; i++)
+        {
+            int index = i;
+
+            GameObject buttonObj = Instantiate(buttonPrefab, buttonContainer);
+
+            // Set size manually
+            RectTransform rect = buttonObj.GetComponent<RectTransform>();
+            rect.sizeDelta = buttonSize;
+
+            // Set position manually
+            rect.anchoredPosition = new Vector2(0f, startY);
+            startY -= buttonSize.y + 10f;
+
+            buttonObj.GetComponentInChildren<TMP_Text>().text = $"Button {i + 1}";
+            buttonObj.GetComponent<Button>().onClick.AddListener(() => SetText(index));
+        }
     }
 
     public void BackToMenu()
@@ -41,36 +69,48 @@ public class Instructions : MonoBehaviour
 
     public void SetText(int index)
     {
-        string newText;
-
-        switch (index)
+        if (index >= 0 && index < _instructions.Count)
         {
-            case 0:
-                newText = _instructions[0];
-                break;
-            case 1:
-                newText = _instructions[1];
-                break;
-            case 2:
-                newText = _instructions[2];
-                break;
-            case 3:
-                newText = _instructions[3];
-                break;
-            case 4:
-                newText = _instructions[4];
-                break;
-            case 5:
-                newText = _instructions[5];
-                break;
-            case 6:
-                newText = _instructions[6];
-                break;
-            default:
-                newText = "Instruction not found.";
-                break;
+            instructionText.GetComponent<TMP_Text>().text = _instructions[index];
         }
-
-        instructionText.GetComponent<TMP_Text>().text = newText;
+        else
+        {
+            instructionText.GetComponent<TMP_Text>().text = "Instruction not found.";
+        }
     }
+
+    // public void SetText(int index)
+    // {
+    //     string newText;
+
+    //     switch (index)
+    //     {
+    //         case 0:
+    //             newText = _instructions[0];
+    //             break;
+    //         case 1:
+    //             newText = _instructions[1];
+    //             break;
+    //         case 2:
+    //             newText = _instructions[2];
+    //             break;
+    //         case 3:
+    //             newText = _instructions[3];
+    //             break;
+    //         case 4:
+    //             newText = _instructions[4];
+    //             break;
+    //         case 5:
+    //             newText = _instructions[5];
+    //             break;
+    //         case 6:
+    //             newText = _instructions[6];
+    //             break;
+    //         default:
+    //             newText = "Instruction not found.";
+    //             break;
+    //     }
+
+    //     instructionText.GetComponent<TMP_Text>().text = newText;
+    // }
 }
