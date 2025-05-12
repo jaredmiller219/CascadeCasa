7a55a735cd8dbe26d09beb2210ff5157febcee05
Jared Miller <jaredmiller219@gmail.com>
2025-05-12 18:05:21 -0400

    fixing some bugs in the level

--- Assets/Scripts/Animation/LivingRoom/LivingRoom_ResetDropdown.cs
+++ Assets/Scripts/Animation/LivingRoom/LivingRoom_ResetDropdown.cs
@@ -43,7 +43,7 @@ public class LivingRoom_ResetPopup : MonoBehaviour
         if (audioSource && popupSound) audioSource.PlayOneShot(popupSound);
         if (resetPopup == null || _animator == null || notepad == null) return;
 
-        if (notepad.inputField.GetComponent<TMPro.TMP_InputField>().text != "")
+        if (GetNotepadText(notepad.inputField) != "")
         {
             resetPopup.SetActive(true);
             _animator.Play("Pull", 0, 0f);
@@ -63,5 +63,17 @@ public class LivingRoom_ResetPopup : MonoBehaviour
         yield return new WaitForSeconds(animationLength);
         _animator.Play("Pull", 0, 0f);
         resetPopup.SetActive(false);
+    }
+
+    /// <summary>
+    /// Gets the text of the notepad's input field and returns it as a string
+    /// </summary>
+    /// <param name="notepad">The notepad gameobject</param>
+    /// <returns>The text of the component as a string</returns>
+    private string GetNotepadText(GameObject notepad)
+    {
+        if (notepad == null) return string.Empty;
+        if (!notepad.TryGetComponent<TMPro.TMP_InputField>(out var inputField)) return string.Empty;
+        return inputField.text;
     }
 }
