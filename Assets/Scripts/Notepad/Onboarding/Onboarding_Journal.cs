0a610869d4f6afb07c6901b988487d00f6628b00
Dustin Tran <dustintr@udel.edu>
2025-05-19 19:14:54 -0400

    Making onboarding a seperate level

---
+++ Assets/Scripts/Notepad/Onboarding/Onboarding_Journal.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Onboarding_Journal : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private static readonly int Hover = Animator.StringToHash("hover");
+
+    /// <summary>
+    /// Reference to the journal popup GameObject that appears when the journal button is clicked.
+    /// </summary>
+    public GameObject journalPopup;
+
+    /// <summary>
+    /// The source of the audio
+    /// </summary>
+    public AudioSource audioSource;
+
+    /// <summary>
+    /// The audio sound
+    /// </summary>
+    public AudioClip clickSound;
+
+    /// <summary>
+    /// Reference to the journal button GameObject.
+    /// </summary>
+    [SerializeField]
+    private Button journalButton;
+
+    /// <summary>
+    /// Reference to the animator component attached to the journal button.
+    /// </summary>
+    private Animator animator;
+
+    /// <summary>
+    /// Reference to the GameObject representing the journal image.
+    /// </summary>
+    [SerializeField]
+    private GameObject JournalImage;
+
+    /// <summary>
+    /// A reference to the living room notepad
+    /// </summary>
+    [SerializeField]
+    private LivingRoom_Notepad notepad;
+
+    private void Start()
+    {
+        if (journalPopup) journalPopup.SetActive(false);
+        animator = journalButton.GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Toggles the visibility of the journal popup.
+    /// <br />
+    /// This method is called when the journal button is clicked.
+    /// </summary>
+    /// <param name="isActive">True to show the journal popup, false to hide it.</param>
+    public void ToggleJournal()
+    {
+        notepad.SaveCurrentInputIfNeeded();
+        journalPopup.SetActive(!journalPopup.activeSelf);
+    }
+
+    /// <summary>
+    /// Sets the hover state of the journal button.
+    /// </summary>
+    /// <param name="isHovering">True if the mouse is hovering over the button, false otherwise.</param>
+    public void SetHover(bool isHovering)
+    {
+        if (animator) animator.SetBool(Hover, isHovering);
+    }
+
+    /// <summary>
+    /// Handles the pointer down event when the button is pressed.
+    /// This method changes the color of the JournalImage to indicate a pressed state.
+    /// </summary>
+    /// <param name="eventData">The event data associated with the pointer down event.</param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Light gray
+        if (JournalImage) JournalImage.GetComponent<Image>().color = new Color32(200, 200, 200, 255);
+    }
+
+    /// <summary>
+    /// Handles the pointer up event when the button is released.
+    /// <br />
+    /// This method changes the color of the JournalImage back to white.
+    /// </summary>
+    /// <param name="eventData">The event data associated with the pointer up event.</param>
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // White
+        if (JournalImage) JournalImage.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        if (audioSource && clickSound) audioSource.PlayOneShot(clickSound);
+    }
+}
